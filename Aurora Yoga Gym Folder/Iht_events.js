1777812a408c9171b48d4ffdf75a65853f52722d
BlyssM <blyss.marte@mail.citytech.cuny.edu>
2026-05-17 16:24:38 -0400

    Add files via upload

---
+++ Aurora Yoga Gym Folder/Iht_events.js
@@ -0,0 +1,44 @@
+"use strict"
+/* JavaScript for Events page 
+Created by: Blyss Marte 
+Date: May 17, 2026*/
+
+var dayEvent = new Array();
+
+dayEvent[1] = "<br /><a href='#'>Yoga and Meditation Workshop</a><br />10 am - 12 pm<br />$20"; 
+dayEvent[2] = "<br /><a href='#'>Outdoor Yoga Session</a><br />6 pm - 7 pm<br />Free for members";
+dayEvent[3] = "<br /><a href='#'>Yoga for Beginners</a><br />5 pm - 6 pm<br />$15";
+dayEvent[4] = "<br /><a href='#'>Power Yoga Class</a><br />7 pm - 8 pm<br />$18";
+dayEvent[5] = "<br /><a href='#'>Restorative Yoga Workshop</a><br />10 am - 12 pm<br />$20";
+dayEvent[6] = "<br /><a href='#'>Community Yoga Gathering</a><br />5 pm - 6 pm<br />Free for members";
+dayEvent[7] = "<br /><a href='#'>Yoga and Nutrition Seminar</a><br />6 pm - 7:30 pm<br />$25";
+
+dayEvent[8] = "<br /><a href='#'>Sunrise Yoga Session</a><br />6 am - 7 am<br />Free for members";
+dayEvent[9] = "<br /><a href='#'>Yoga for Stress Relief</a><br />5 pm - 6 pm<br />$15";
+dayEvent[10] = "<br /><a href='#'>Vinyasa Flow Class</a><br />7 pm - 8 pm<br />$18";
+dayEvent[11] = "<br /><a href='#'>Yoga and Mindfulness Workshop</a><br />10 am - 12 pm<br />$20";
+dayEvent[12] = "";
+dayEvent[13] = "<br /><a href='#'>Yoga for Back Pain Relief</a><br />6 pm - 7 pm<br />$15";
+dayEvent[14] = "<br /><a href='#'>Yoga and Aromatherapy Seminar</a><br />6 pm - 7:30 pm<br />$25";
+
+dayEvent[15] = "<br /><a href='#'>Power Yoga Class</a><br />7 pm - 8 pm<br />$18";
+dayEvent[16] = "<br /><a href='#'>Restorative Yoga Workshop</a><br />10 am - 12 pm<br />$20";
+dayEvent[17] = "<br /><a href='#'>Community Yoga Gathering</a><br />5 pm - 6 pm<br />Free for members";
+
+dayEvent[18] = "<br /><a href='#'>Yoga and Nutrition Seminar</a><br />6 pm - 7:30 pm<br />$25";
+dayEvent[19] = "<br /><a href='#'>Sunrise Yoga Session</a><br />6 am - 7 am<br />Free for members";
+dayEvent[20] = "<br /><a href='#'>Yoga for Stress Relief</a><br />5 pm - 6 pm<br />$15";
+dayEvent[21] = "<br /><a href='#'>Vinyasa Flow Class</a><br />7 pm - 8 pm<br />$18";
+
+dayEvent[22] = "<br /><a href='#'>Yoga and Mindfulness Workshop</a><br />10 am - 12 pm<br />$20";
+dayEvent[23] = "<br /><a href='#'>Yoga for Back Pain Relief</a><br />6 pm - 7 pm<br />$15";
+dayEvent[24] = "<br /><a href='#'>Yoga and Aromatherapy Seminar</a><br />6 pm - 7:30 pm<br />$25";
+
+dayEvent[25] = "";
+dayEvent[26] = "<br /><a href='#'>Outdoor Yoga Session</a><br />6 pm - 7 pm<br />Free for members";
+dayEvent[27] = "";
+dayEvent[28] = "<br /><a href='#'>Power Yoga Class</a><br />7 pm - 8 pm<br />$18";
+dayEvent[29] = "<br /><a href='#'>Restorative Yoga Workshop</a><br />10 am - 12 pm<br />$20";
+dayEvent[30] = "<br /><a href='#'>Community Yoga Gathering</a><br />5 pm - 6 pm<br />Free for members";
+
+dayEvent[31] = "<br /><a href='#'>Yoga and Nutrition Seminar</a><br />6 pm - 7:30 pm<br />$25";
